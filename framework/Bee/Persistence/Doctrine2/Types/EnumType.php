299893c03ace49285ab7de1e47cbe4251d1a2840
Michael Plomer <mplomer@users.noreply.github.com>
2013-08-24 12:32:07 +0000

    - restructured libs - added Doctrine2 EnumType

---
+++ framework/Bee/Persistence/Doctrine2/Types/EnumType.php
@@ -0,0 +1,39 @@
+<?php
+namespace Bee\Persistence\Doctrine2\Types;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Class EnumType
+ * @package Bee\Persistence\Doctrine2\Types
+ */
+abstract class EnumType extends Type
+{
+    protected $name;
+    protected $values = array();
+
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        $values = array_map(function($val) { return "'".$val."'"; }, $this->values);
+
+        return "ENUM(".implode(", ", $values).") COMMENT '(DC2Type:".$this->name.")'";
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (!in_array($value, $this->values)) {
+            throw new \InvalidArgumentException("Invalid '".$this->name."' value.");
+        }
+        return $value;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
