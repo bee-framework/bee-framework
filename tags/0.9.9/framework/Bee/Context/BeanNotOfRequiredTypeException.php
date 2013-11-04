cc2db3a7fd52ed9371b60f069e828fdf6cbf70b5
m_plomer <m_plomer@529412d7-8917-41f1-b15e-85b327bc79eb>
2013-11-04 15:24:57 +0000

    - tagged latest changes

---
+++ tags/0.9.9/framework/Bee/Context/BeanNotOfRequiredTypeException.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Enter description here...
+ *
+ * @author Benjamin Hartmann
+ * @author Michael Plomer <michael.plomer@iter8.de>
+ */
+class Bee_Context_BeanNotOfRequiredTypeException extends Bee_Context_BeansException {
+	
+	const EXCEPTION_MESSAGE = 'Bean with name %s has type %s (should be %s).';
+	
+	private $name;
+	private $requiredType;
+	private $actualType;
+	
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @param String $name
+	 * @param String $requiredType
+	 * @param String $actualType
+	 * @return void
+	 */
+	public function __construct($name, $requiredType, $actualType=null) {
+		parent::__construct(sprintf(self::EXCEPTION_MESSAGE, $name, $actualType, $requiredType));
+		$this->name = $name;
+		$this->requiredType = $requiredType;
+		$this->actualType = $actualType;
+	}
+	
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @return String
+	 */
+	public function getBeanName() {
+		return $this->name;
+	}
+
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @return String
+	 */
+	public function getRequiredType() {
+		return $this->requiredType;
+	}
+
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @return String
+	 */
+	public function getActualType() {
+		return $this->actualType;
+	}
+}
+
+?>
---
+++ tags/0.9.9/framework/Bee/MVC/HandlerMapping/AntPath.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Enter description here...
+ *
+ * @author Michael Plomer <michael.plomer@iter8.de>
+ */
+class Bee_MVC_HandlerMapping_AntPath extends Bee_MVC_HandlerMapping_Abstract {
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @var array
+	 */
+	private $handlerMappings;
+
+	public function getHandlerMappings() {
+		return $this->handlerMappings;
+	}
+	
+	public function setHandlerMappings($handlerMappings) {
+		$this->handlerMappings = $handlerMappings;
+	}
+	
+	protected function getControllerBeanName(Bee_MVC_IHttpRequest $request) {
+		$matcher = new Bee_Utils_AntPathMatcher();
+//		$pathInfo = Bee_Utils_Env::getPathInfo();
+		$pathInfo = $request->getPathInfo();
+		
+		$controllerBeanName = $this->getDefaultControllerBeanName();
+		
+		if (array_key_exists($pathInfo, $this->handlerMappings)) {
+			// shortcut for direct path matches
+			$controllerBeanName = $this->handlerMappings[$pathInfo];
+		} else {
+			foreach($this->handlerMappings as $mapping => $handler) {
+				if($matcher->match($mapping, $pathInfo)) {
+//				if(($matcher->isPattern($mapping) && $matcher->match($mapping, $pathInfo)) || Bee_Utils_Strings::startsWith($pathInfo, $mapping)) {
+					$controllerBeanName = $handler;
+					break;
+				}
+			}
+		}
+
+		return $controllerBeanName;
+	}
+}
+?>
