3f39c63362589f2103461a8b3de3ff626850adda
m_plomer <m_plomer@529412d7-8917-41f1-b15e-85b327bc79eb>
2013-11-07 17:19:15 +0000

    tagged latest changes

---
+++ tags/0.9.15/framework/Bee/Context/Config/CompositeComponentDefinition.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * User: mp
+ * Date: Feb 18, 2010
+ * Time: 7:11:18 AM
+ */
+
+class Bee_Context_Config_CompositeComponentDefinition extends Bee_Context_Config_AbstractComponentDefinition {
+
+    private $name;
+
+    private $nestedComponents = array();
+
+
+    /**
+     * Create a new CompositeComponentDefinition.
+     * @param name the name of the composite component
+     * @param source the source element that defines the root of the composite component
+     */
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * Add the given component as nested element of this composite component.
+     * @param component the nested component to add
+     */
+    public function addNestedComponent(Bee_Context_Config_IComponentDefinition $component) {
+        $this->nestedComponents[] = $component;
+    }
+
+    /**
+     * Return the nested components that this composite component holds.
+     * @return Bee_Context_Config_IComponentDefinition[] the array of nested components, or an empty array if none
+     */
+    public function getNestedComponents() {
+        return $this->nestedComponents;
+    }
+}
+?>
