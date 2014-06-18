fa9db1317795958e86f9f466aff8c19a9b15fee1
m_plomer <m_plomer@529412d7-8917-41f1-b15e-85b327bc79eb>
2014-06-18 12:24:38 +0000

    tagged latest changes

---
+++ tags/0.9.26/framework/Bee/MVC/Model.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Enter description here...
+ * 
+ * @author Benjamin Hartmann
+ * @author Michael Plomer <michael.plomer@iter8.de>
+ */
+class Bee_MVC_Model {
+	
+	const CURRENT_REQUEST_KEY = '__CURRENT_REQUEST';
+	
+	private static $modelValues = array();	
+	
+	/**
+	 * Add the values from the given (associative) array to the model.
+	 *
+	 * @param array $values
+	 */
+	public static function addValuesToModel(Array $values) {
+		self::$modelValues = array_merge(self::$modelValues, $values);
+	}
+
+	/**
+	 * Add a single value to the model under the given key
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	public static function putValue($key, $value) {
+		self::$modelValues[$key] = $value;
+	}
+
+	/**
+	 * Clear the model, removing all 
+	 *
+	 */
+	public static function clear() {
+		self::$modelValues = array();
+	}
+	
+	public static function getValue($key) {
+		return self::$modelValues[$key];
+	}
+	
+	public static function hasValue($key) {
+		return array_key_exists($key, self::$modelValues);
+	}
+
+	public static function getModelValues() {
+		return self::$modelValues;
+	}
+}
+
+
+/**
+ * This class is a convenience for accessing 
+ *
+ */
+class MODEL extends Bee_MVC_Model {
+	
+	public static function get($key, $defaultValue=null) {
+        if (!is_null($defaultValue) && !Bee_MVC_Model::hasValue($key)) {
+            return $defaultValue;
+        }
+		return Bee_MVC_Model::getValue($key);
+	}
+
+	public static function getModel() {
+		return Bee_MVC_Model::getModelValues();
+	}
+}
---
+++ tags/0.9.26/framework/Bee/Security/Config/MethodSecurityInterceptorPostProcessor.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * User: mp
+ * Date: Feb 19, 2010
+ * Time: 10:29:38 PM
+ */
+
+class Bee_Security_Config_MethodSecurityInterceptorPostProcessor implements Bee_Context_Config_IBeanPostProcessor, Bee_Context_Config_IContextAware {
+
+    /**
+     * @var Bee_IContext      
+     */
+    private $beeContext;
+
+    public function postProcessBeforeInitialization($bean, $beanName) {
+
+        if(Bee_Security_Config_IBeanIds::METHOD_SECURITY_INTERCEPTOR == $beanName &&
+                $this->beeContext->containsBean(Bee_Security_Config_IBeanIds::AFTER_INVOCATION_MANAGER)) {
+            Bee_Utils_Logger::debug("Setting AfterInvocationManaer on MethodSecurityInterceptor");
+            $bean->setAfterInvocationManager($this->beeContext->getBean(Bee_Security_Config_IBeanIds::AFTER_INVOCATION_MANAGER));
+        }
+
+        return $bean;
+    }
+
+    public function postProcessAfterInitialization($bean, $beanName) {
+        return $bean;
+    }
+
+    public function setBeeContext(Bee_IContext $context) {
+        $this->beeContext = $context;
+    }
+
+}
+?>
