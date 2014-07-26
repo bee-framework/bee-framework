f3db544716e9eb3d1f9ecb0315bbb997825ea402
m_plomer <m_plomer@529412d7-8917-41f1-b15e-85b327bc79eb>
2014-07-26 16:32:06 +0000

    Removed tags/0.2.2-beta

--- tags/0.2.2-beta/examples/redirected.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../vendor/autoload.php';
-session_start();
-
-$requestBuilder = new \Bee\MVC\Redirect\RedirectedRequestBuilder();
-$request = $requestBuilder->buildRequestObject();
-
-var_dump($_REQUEST);
-var_dump($request);
