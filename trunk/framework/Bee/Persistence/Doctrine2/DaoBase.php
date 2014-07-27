4819306ea43d3366c937708afa02161166f716fe
m_plomer <m_plomer@529412d7-8917-41f1-b15e-85b327bc79eb>
2014-07-27 12:44:42 +0000

    - minor refactorings, especially in Doctrine 2 persistence APIs

--- trunk/framework/Bee/Persistence/Doctrine2/DaoBase.php
+++ trunk/framework/Bee/Persistence/Doctrine2/DaoBase.php
@@ -1,12 +1,26 @@
 <?php
 namespace Bee\Persistence\Doctrine2;
+/*
+ * Copyright 2008-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 use Bee_Persistence_IOrderAndLimitHolder;
 use Bee_Persistence_IRestrictionHolder;
 use Bee_Utils_Strings;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
 use Exception;
-use Logger;
 
 /**
  * User: mp
@@ -14,21 +28,6 @@ use Logger;
  * Time: 17:26
  */
 class DaoBase extends EntityManagerHolder {
-
-	/**
-	 * @var Logger
-	 */
-	protected $log;
-
-	/**
-	 * @return Logger
-	 */
-	protected function getLog() {
-		if (!$this->log) {
-			$this->log = Logger::getLogger(get_class($this));
-		}
-		return $this->log;
-	}
 
 	/**
 	 * @param QueryBuilder $queryBuilder
--- trunk/framework/Bee/Persistence/Doctrine2/EntityManagerHolder.php
+++ trunk/framework/Bee/Persistence/Doctrine2/EntityManagerHolder.php
@@ -1,7 +1,7 @@
 <?php
 namespace Bee\Persistence\Doctrine2;
 /*
- * Copyright 2008-2010 the original author or authors.
+ * Copyright 2008-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@ namespace Bee\Persistence\Doctrine2;
  * limitations under the License.
  */
 use \Doctrine\ORM\EntityManager;
+use Logger;
 
 /**
  * User: mp
@@ -24,6 +25,21 @@ use \Doctrine\ORM\EntityManager;
  */
  
 class EntityManagerHolder {
+
+	/**
+	 * @var Logger
+	 */
+	protected $log;
+
+	/**
+	 * @return Logger
+	 */
+	public function getLog() {
+		if (!$this->log) {
+			$this->log = Logger::getLogger(get_class($this));
+		}
+		return $this->log;
+	}
 
 	/**
 	 * @var EntityManager
