18da9711f97ef64a9f13967ae7129c7ffb6b8309
m_plomer <m_plomer@529412d7-8917-41f1-b15e-85b327bc79eb>
2014-09-10 17:13:01 +0000

    - D2 EnumType: fixed compatibility with early 5.3 version (which exhibit quirks in reflected access to late statically bound methods)

--- trunk/framework/Bee/Persistence/Doctrine2/Types/EnumType.php
+++ trunk/framework/Bee/Persistence/Doctrine2/Types/EnumType.php
@@ -27,11 +27,6 @@ abstract class EnumType extends Type {
 	const ENUM_BASE_TYPE = 'Bee\Utils\EnumBase';
 
 	/**
-	 * @var \ReflectionClass
-	 */
-	private $reflClass;
-
-	/**
 	 * @return string
 	 */
 	protected static function getEnumClassName() {
@@ -39,7 +34,6 @@ abstract class EnumType extends Type {
 	}
 
 	public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform) {
-		if (!$this->reflClass) self::init();
 		$values = array_map(function ($val) {
 			return "'" . $val . "'";
 		}, call_user_func(array(static::getEnumClassName(), 'getValues')));
@@ -47,29 +41,20 @@ abstract class EnumType extends Type {
 	}
 
 	public function convertToPHPValue($value, AbstractPlatform $platform) {
-		if (!$this->reflClass) self::init();
-		return $this->reflClass->getMethod('get')->invoke(null, $value);
+		return call_user_func(array(static::getEnumClassName(), 'get'));
 	}
 
 	public function convertToDatabaseValue($value, AbstractPlatform $platform) {
-		if (!$this->reflClass) self::init();
-		if(is_null($val)) {
+		if(is_null($value)) {
 			return $value;
 		}
-		if (!$this->reflClass->isInstance($value)) {
+		if (get_class($value) != static::getEnumClassName()) {
 			throw new \UnexpectedValueException('Not a valid enum element for "' . static::getEnumClassName() . '": ' . $value);
 		}
 		// check if value is valid
 		self::convertToPHPValue($value->val(), $platform);
 		// return actual value
 		return $value->val();
-	}
-
-	private function init() {
-		$this->reflClass = new \ReflectionClass(static::getEnumClassName());
-		if (!$this->reflClass->isSubclassOf(self::ENUM_BASE_TYPE)) {
-			throw new \UnexpectedValueException('"' . $this->reflClass . '" is not a subclass of "' . self::ENUM_BASE_TYPE . '"');
-		}
 	}
 
 	public function getName() {
