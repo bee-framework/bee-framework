105196ea8184eee052acbc3802ac870202f34cc0
Michael Plomer <mplomer@users.noreply.github.com>
2014-10-31 07:35:38 +0000

    - backport of proper Doctrine2 pagination from trunk

--- framework/Bee/Persistence/IOrderAndLimitHolder.php
+++ framework/Bee/Persistence/IOrderAndLimitHolder.php
@@ -1,5 +1,23 @@
 <?php
+/*
+ * Copyright 2008-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+/**
+ * Interface Bee_Persistence_IOrderAndLimitHolder
+ */
 interface Bee_Persistence_IOrderAndLimitHolder {
 
     /**
@@ -18,11 +36,6 @@ interface Bee_Persistence_IOrderAndLimitHolder {
     public function getPageCount();
 
     /**
-     * @param $pageCount
-     */
-    public function setPageCount($pageCount);
-
-    /**
      * @return int
      */
     public function getCurrentPage();
@@ -32,6 +45,8 @@ interface Bee_Persistence_IOrderAndLimitHolder {
      */
     public function setCurrentPage($currentPage);
 
+	/**
+	 * @param int $resultCount
+	 */
+	public function setResultCount($resultCount);
 }
-
-?>
