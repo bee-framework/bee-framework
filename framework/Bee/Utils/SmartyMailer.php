932221e4a60f1c70015a281609d1cab60e8f8170
Michael Plomer <michael.plomer@iter8.de>
2015-03-18 06:45:39 +0100

    - SmartyMailer: added altBodyTemplate parameter to sendMail() - Env: added methods to get base URL

--- framework/Bee/Utils/SmartyMailer.php
+++ framework/Bee/Utils/SmartyMailer.php
@@ -1,5 +1,6 @@
 <?php
 namespace Bee\Utils;
+
 /*
  * Copyright 2008-2014 the original author or authors.
  *
@@ -27,52 +28,54 @@ class SmartyMailer extends PhpMailerWrapper {
 
     private $pluginDir;
 
-	/**
-	 * 
-	 * @var Smarty
-	 */
-	private $smarty;
+    /**
+     *
+     * @var Smarty
+     */
+    private $smarty;
 
-	/**
-	 * @var string
-	 */
-	private $messageLogFile;
+    /**
+     * @var string
+     */
+    private $messageLogFile;
 
-	/**
-	 * @param $subjectTemplate
-	 * @param $bodyTemplate
-	 * @param array $model
-	 * @param mixed $recipient Either string example@mail.com or array with keys "address" and optional "name"
-	 * @param mixed $sender Either string example@mail.com or array with keys "address" and optional "name"
-	 * @throws Exception
-	 * @throws phpmailerException
-	 * @return bool
-	 */
-    public function sendMail($subjectTemplate, $bodyTemplate, array $model, $recipient=null, $sender=null) {
-        $phpMailer = $this->instantiatePhpMailer($subjectTemplate, $bodyTemplate, $model, $recipient, $sender);
+    /**
+     * @param $subjectTemplate
+     * @param $bodyTemplate
+     * @param array $model
+     * @param mixed $recipient Either string example@mail.com or array with keys "address" and optional "name"
+     * @param mixed $sender Either string example@mail.com or array with keys "address" and optional "name"
+     * @param string $altBodyTemplate
+     *
+     * @throws Exception
+     * @throws phpmailerException
+     * @return bool
+     */
+    public function sendMail($subjectTemplate, $bodyTemplate, array $model, $recipient = null, $sender = null, $altBodyTemplate = null) {
+        $phpMailer = $this->instantiatePhpMailer($subjectTemplate, $bodyTemplate, $model, $recipient, $sender, $altBodyTemplate);
 
         if (!$phpMailer->Send()) {
             throw new Exception($phpMailer->ErrorInfo);
         }
         return true;
-	}
+    }
 
-	/**
-	 * @param $subjectTemplate
-	 * @param $bodyTemplate
-	 * @param array $model
-	 * @param null $recipient
-	 * @param null $sender
-	 */
-	public function logMail($subjectTemplate, $bodyTemplate, array $model, $recipient=null, $sender=null) {
-		$phpMailer = $this->instantiatePhpMailer($subjectTemplate, $bodyTemplate, $model, $recipient, $sender);
-		if($this->messageLogFile && $phpMailer->preSend()) {
-			$message = $phpMailer->getMailMIME() . "\n\n";
-			$message .= $phpMailer->Subject . "\n\n";
-			$message .= $phpMailer->Body . "\n\n";
-			file_put_contents($this->messageLogFile, $message);
-		}
-	}
+    /**
+     * @param $subjectTemplate
+     * @param $bodyTemplate
+     * @param array $model
+     * @param null $recipient
+     * @param null $sender
+     */
+    public function logMail($subjectTemplate, $bodyTemplate, array $model, $recipient = null, $sender = null) {
+        $phpMailer = $this->instantiatePhpMailer($subjectTemplate, $bodyTemplate, $model, $recipient, $sender);
+        if ($this->messageLogFile && $phpMailer->preSend()) {
+            $message = $phpMailer->getMailMIME() . "\n\n";
+            $message .= $phpMailer->Subject . "\n\n";
+            $message .= $phpMailer->Body . "\n\n";
+            file_put_contents($this->messageLogFile, $message);
+        }
+    }
 
     /**
      * @param $subjectTemplate
@@ -81,17 +84,17 @@ class SmartyMailer extends PhpMailerWrapper {
      * @param mixed $recipient Either string example@mail.com or array with keys "address" and optional "name"
      * @param mixed $sender Either string example@mail.com or array with keys "address" and optional "name"
      */
-    public function dumpMail($subjectTemplate, $bodyTemplate, array $model, $recipient=null, $sender=null, $exit=true) {
+    public function dumpMail($subjectTemplate, $bodyTemplate, array $model, $recipient = null, $sender = null, $exit = true) {
         $phpMailer = $this->instantiatePhpMailer($subjectTemplate, $bodyTemplate, $model, $recipient, $sender);
         // TODO: inlude bcc and cc in wrapper!
         $phpMailer->AddBCC('b.hartmann@arend-hartmann.com', 'Benjamin Hartmann');
 
         echo '<div style="border: solid 1px #f00; padding: 5px; margin: 10px;">';
         echo 'To:<br/>';
-        echo '=> Address: '.$recipient['address'].'<br/>';
-        echo '=> Name: '.$recipient['name'].'<br/>';
+        echo '=> Address: ' . $recipient['address'] . '<br/>';
+        echo '=> Name: ' . $recipient['name'] . '<br/>';
         echo '<br/>';
-        echo 'Subject: "'.$phpMailer->Subject.'"<br/>';
+        echo 'Subject: "' . $phpMailer->Subject . '"<br/>';
         echo '</div>';
 
         echo '<div style="border: solid 1px #f00; padding: 5px; margin: 10px;">';
@@ -101,29 +104,34 @@ class SmartyMailer extends PhpMailerWrapper {
         if ($exit) {
             exit();
         }
-	}
+    }
 
 
-	/**
-	 * @param $subjectTemplate
-	 * @param $bodyTemplate
-	 * @param array $model
-	 * @param mixed $recipient Either string example@mail.com or array with keys "address" and optional "name"
-	 * @param mixed $sender Either string example@mail.com or array with keys "address" and optional "name"
-	 *
-	 * @throws Exception
-	 * @return \PHPMailer
-	 */
-    public function instantiatePhpMailer($subjectTemplate, $bodyTemplate, array $model, $recipient=null, $sender=null) {
-		$phpMailer = $this->createMailer($sender, $recipient);
+    /**
+     * @param string $subjectTemplate
+     * @param string $bodyTemplate
+     * @param array $model
+     * @param mixed $recipient Either string example@mail.com or array with keys "address" and optional "name"
+     * @param mixed $sender Either string example@mail.com or array with keys "address" and optional "name"
+     * @param string $altBodyTemplate
+     *
+     * @throws Exception
+     * @return \PHPMailer
+     */
+    public function instantiatePhpMailer($subjectTemplate, $bodyTemplate, array $model, $recipient = null, $sender = null, $altBodyTemplate = null) {
+        $phpMailer = $this->createMailer($sender, $recipient);
 
         // SET CONTENT
         $this->smarty->clearAllAssign();
-        foreach($model as $key => $value) {
+        foreach ($model as $key => $value) {
             $this->smarty->assign($key, $value);
         }
         $phpMailer->Subject = $this->smarty->fetch($subjectTemplate);
         $phpMailer->Body = $this->smarty->fetch($bodyTemplate);
+
+        if(!is_null($altBodyTemplate)) {
+            $phpMailer->AltBody = $this->smarty->fetch($altBodyTemplate);
+        }
 
         return $phpMailer;
     }
@@ -149,33 +157,33 @@ class SmartyMailer extends PhpMailerWrapper {
     }
 
     /**
-	 * 
-	 * @return Smarty
-	 */
-	public final function getSmarty() {
-		return $this->smarty;
-	}
+     *
+     * @return Smarty
+     */
+    public final function getSmarty() {
+        return $this->smarty;
+    }
 
-	/**
-	 * 
-	 * @param Smarty $smarty
-	 * @return void
-	 */
-	public final function setSmarty(Smarty $smarty) {
-		$this->smarty = $smarty;
-	}
+    /**
+     *
+     * @param Smarty $smarty
+     * @return void
+     */
+    public final function setSmarty(Smarty $smarty) {
+        $this->smarty = $smarty;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getMessageLogFile() {
-		return $this->messageLogFile;
-	}
+    /**
+     * @return string
+     */
+    public function getMessageLogFile() {
+        return $this->messageLogFile;
+    }
 
-	/**
-	 * @param string $messageLogFile
-	 */
-	public function setMessageLogFile($messageLogFile) {
-		$this->messageLogFile = $messageLogFile;
-	}
+    /**
+     * @param string $messageLogFile
+     */
+    public function setMessageLogFile($messageLogFile) {
+        $this->messageLogFile = $messageLogFile;
+    }
 }
